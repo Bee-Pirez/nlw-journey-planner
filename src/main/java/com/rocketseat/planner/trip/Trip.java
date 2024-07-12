8bb342b50a125f3d4e7f4f6f2d46204b0e7c4624
Beatriz <beatrizcarmoperez@gmail.com>
2024-07-11 23:28:49 -0300

    feat: create new repository

--- src/main/java/com/rocketseat/planner/trip/Trip.java
+++ src/main/java/com/rocketseat/planner/trip/Trip.java
@@ -1,7 +1,31 @@
 package com.rocketseat.planner.trip;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 public class Trip {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String destination;
+
+    @Column(name = "starts_at",nullable = false)
+    private LocalDateTime starstAt;
+
+    @Column(name = "ends_at",nullable = false)
+    private LocalDateTime endstAt;
+
+    @Column(name = "is_confirmed",nullable = false)
+    private Boolean isConfirmed;
+
+    @Column(name = "owner_name",nullable = false)
+    private String ownerName;
+
+    @Column(name = "owner_email",nullable = false)
+    private String ownerEmail;
 }
